11fce005f46357297188adb9b4e91311e099d946
huoxz88 <195819264+huoxz88@users.noreply.github.com>
2025-08-05 00:26:54 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,8 +4,8 @@ import re
 import os
 
 # 目标URL列表
-urls = ['https://stock.hostmonit.com/CloudFlareYes', 
-        'https://cf.090227.xyz/'
+urls = ['https://api.uouin.com/cloudflare.html', 
+        'https://ip.164746.xyz/'
         ]
 
 # 正则表达式用于匹配IP地址
@@ -25,9 +25,9 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://stock.hostmonit.com/CloudFlareYes':
+        if url == 'https://api.uouin.com/cloudflare.html':
             elements = soup.find_all('tr')
-        elif url == 'https://cf.090227.xyz//':
+        elif url == 'https://ip.164746.xyz/':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
